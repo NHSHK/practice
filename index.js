c23f859b360c60e2db108e855dd8db3b19afca08
Jongcheol Park <pjc514@naver.com>
2023-06-12 00:22:17 +0900

    cat

--- index.js
+++ index.js
@@ -5,25 +5,32 @@ const listItems = document.querySelectorAll(".list-item");
 const division =
   (listStyleChangeEndY - listStyleChangeStartY) / listItems.length;
 
+const windowHeight = document.documentElement.clientHeight;
+
 const panel1Img = document.getElementById("panel1-img");
 const flyingSantaImg = document.getElementById("flying-santa-img");
 
-const videoPlayBack = 50;
+const videoPlayBack = 350;
+const videoSection = document.getElementById("video-section");
+const fixdeWrapper = document.getElementById("fixed-wrapper");
 const videoElement = document.getElementById("video");
 
-function centerElement(elementId) {
+function centerElement(elementId, video) {
   const element = document.getElementById(elementId);
   const parent = element.parentElement;
 
   if (
     window.scrollY >
-    parent.offsetTop -
-      (document.documentElement.clientHeight - element.offsetHeight) / 2
+    parent.offsetTop - (windowHeight - element.offsetHeight) / 2
   ) {
     element.style.position = "fixed";
     element.style.top = "50%";
     element.style.left = "50%";
     element.style.transform = "translate(-50%, -50%)";
+
+    if (video)
+      video.currentTime =
+        (window.scrollY - videoSection.offsetTop) / videoPlayBack;
   } else {
     element.style.position = "initial";
     element.style.top = "initial";
@@ -31,6 +38,10 @@ function centerElement(elementId) {
     element.style.transform = "initial";
   }
 }
+
+videoElement.addEventListener("loadedmetadata", () => {
+  videoSection.style.height = videoElement.duration * videoPlayBack + "px";
+});
 
 window.addEventListener("scroll", () => {
   if (document.getElementById("on"))
@@ -46,7 +57,7 @@ window.addEventListener("scroll", () => {
     if (listItems[targetIndex]) listItems[targetIndex].id = "on";
   }
 
-  const scrollYBottom = window.scrollY + document.documentElement.clientHeight;
+  const scrollYBottom = window.scrollY + windowHeight;
 
   if (
     scrollYBottom > panel1Img.offsetTop &&
@@ -64,10 +75,20 @@ window.addEventListener("scroll", () => {
     flyingSantaImg.style.transform = `translate(${translateX}px, ${translateY}px) rotate(${rotationDegree}deg)`;
   }
 
-  videoElement.addEventListener("loadedmetadata", () => {
-    document.getElementById("video-section").style.height =
-      videoElement.duration * videoPlayBack + "px";
-  });
+  centerElement("fixed-wrapper", videoElement);
 
-  centerElement("fixed-wrapper");
+  if (
+    window.scrollY >
+    videoSection.offsetTop +
+      videoSection.offsetHeight -
+      videoElement.offsetHeight -
+      (windowHeight - videoElement.offsetHeight) / 2
+  ) {
+    fixdeWrapper.style.position = "initial";
+    fixdeWrapper.style.top = "initial";
+    fixdeWrapper.style.left = "initial";
+    fixdeWrapper.style.transform = `translateY(${
+      videoSection.offsetHeight - fixdeWrapper.offsetHeight
+    }px)`;
+  }
 });
